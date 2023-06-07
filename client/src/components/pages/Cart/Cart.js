8f7c539aa70befd5a927611268df2a57a54b1719
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2023-06-07 15:41:26 +0200

    Add Cart component

--- client/src/components/pages/Cart/Cart.js
+++ client/src/components/pages/Cart/Cart.js
@@ -1,26 +1,88 @@
-import { useSelector } from "react-redux";
-import { getAll } from "../../../redux/cartRedux";
-import { Row, Col } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { Container, Card, Form, Button } from "react-bootstrap";
+import { removeProduct, updateQuantity } from "../../../redux/cartRedux";
 
 
 const Cart = () => {
 
-  const productsInCart = useSelector(getAll);
-  console.log(productsInCart);
+  const productsInCart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
+  const handleRemoveProduct = (productId) => {
+    dispatch(removeProduct(productId));
+  };
+
+  const handleQuantityChange = (productId, newQuantity) => {
+    dispatch(updateQuantity(productId, newQuantity));
+  }
 
   return (
-    <div>
-      <h1>Cart</h1>
-      <Row xs={1} md={3} className="g-3">
-        {productsInCart && productsInCart.products.map((product) => (
-          <Col key={product.id}>
-            <h1>{product.name}</h1>
-            {/* Render other product details here */}
-          </Col>
-        ))}
-      </Row>
-    </div>
+    <Container className="d-flex justify-content-center">
+      <Card>
+        <Card.Body>
+          <Card.Title>Cart</Card.Title>
+          {productsInCart.map((product) => {
+            const totalPrice = product.price * product.quantity;
+
+            return (
+            <div key={product.id}>
+              <Card.Text>
+                <b>Name: {product.name}</b>
+              </Card.Text>
+              <Card.Text>
+                <b>Price: {product.price}$</b>
+              </Card.Text>
+              <Form.Group controlId={`description-${product.id}`}>
+                <Form.Label>Comments</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  placeholder="Enter description"
+                />
+              </Form.Group>
+              <Form.Group controlId={`quantity-${product.id}`}>
+                <Form.Label>Quantity</Form.Label>
+                <div className="d-flex">
+                  <Button
+                    variant="dark"
+                    onClick={() => 
+                      handleQuantityChange(product.id, product.quantity - 1)
+                    }
+                  >
+                    -
+                  </Button>
+                  <Form.Control type="number" min="1" value={product.quantity} onChange={(e) => handleQuantityChange(product.id, parseInt(e.target.value))}/>
+                  <Button
+                    variant="dark"
+                    onClick={() =>
+                      handleQuantityChange(product.id, product.quantity + 1)
+                    }
+                  >
+                    +
+                  </Button>
+                </div>
+              </Form.Group>
+              <Card.Text className="mt-2">
+                <b>Total price: {totalPrice}$</b>
+              </Card.Text>
+              <Button
+                variant="danger"
+                onClick={() => handleRemoveProduct(product.id)}
+                className="mt-3"
+              >
+                Remove
+              </Button>
+              <hr />
+            </div>
+            );
+          })}
+          {productsInCart.length === 0 && <p>Your cart is empty.</p>}
+          <Button variant="dark" href="/order-summary">
+            Go to Order Summary
+          </Button>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
 
--- client/src/redux/cartRedux.js
+++ client/src/redux/cartRedux.js
@@ -6,10 +6,12 @@ const createActionName = actionName => `app/cart/${actionName}`;
 
 const ADD_PRODUCT = createActionName('ADD_PRODUCT');
 const REMOVE_PRODUCT = createActionName('REMOVE_PRODUCT');
+const UPDATE_QUANTITY = createActionName('UPDATE_QUANTITY');
 
 // action creators
 export const addProduct = payload => ({ payload, type: ADD_PRODUCT });
 export const removeProduct = payload => ({ payload, type: REMOVE_PRODUCT });
+export const updateQuantity = ( id, quantity ) => ({ payload: { id, quantity }, type: UPDATE_QUANTITY });
 
 
 const cartReducer = (statePart = [], action) => {
@@ -18,6 +20,12 @@ const cartReducer = (statePart = [], action) => {
       return [...statePart, action.payload];
     case REMOVE_PRODUCT: 
       return statePart.filter((product) => product.id !== action.payload);
+    case UPDATE_QUANTITY: {
+      const { id, quantity } = action.payload;
+      return statePart.map(product => 
+        product.id === id ? {...product, quantity} : product
+      );
+    }
     default:
       return statePart;
   };
